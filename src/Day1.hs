acb807918c4831fb870df42ce2d5ad0cc96d57ed
Martín Valdés de León <mvdl@amazon.de>
2021-12-04 21:40:38 +0100

    feat: day 1 part 2

--- src/Day1.hs
+++ src/Day1.hs
@@ -18,9 +18,18 @@ makeTrend (Sonar []) = Sonar []
 count :: Eq a => a -> Sonar a -> Integer
 count a (Sonar as) = toInteger . length $ filter (== a) as
 
+slidingWindow :: Integer -> [a] -> [[a]]
+slidingWindow n xs = transpose (take (fromInteger n) (tails xs))
+
+threeMeasurement :: Num a => Sonar a -> Sonar a
+threeMeasurement (Sonar as) = Sonar $ map sum (slidingWindow 3 as)
+
 day1 :: Text -> IO (String, String)
-day1 input = do
+day1 input =
   let sonar = parseInput input
-  let trend = makeTrend sonar
-  let part1 = count GT trend
-  return (show part1, "N/A")
+      trend = makeTrend sonar
+      part1 = count GT trend
+      sonar' = threeMeasurement sonar
+      trend' = makeTrend sonar'
+      part2 = count GT trend'
+   in return (show part1, show part2)
