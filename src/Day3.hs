647a76406dc6429a7d82dfa25c2cc715f3adfbc4
Martín Valdés de León <mvdl@amazon.de>
2021-12-05 00:46:36 +0100

    feat: day 3 part 2

--- src/Day3.hs
+++ src/Day3.hs
@@ -5,6 +5,7 @@ where
 
 import Control.Lens
 import Data.Char (digitToInt)
+import Data.List ((!!))
 import Numeric (readInt)
 import Relude
 
@@ -38,9 +39,34 @@ readBin number =
     Just dec -> dec
     Nothing -> error $ "Unable to parse binary number: " <> toText number
 
+refine :: String -> Integer -> [String] -> [String]
+refine bitCriteria bitPosition = filter match
+  where
+    match number = number !! fromInteger bitPosition == bitCriteria !! fromInteger bitPosition
+
+data Rating = O2Generator | CO2Scrubber
+  deriving (Show)
+
+findRating :: Rating -> [String] -> String
+findRating rating report = go 0 report
+  where
+    go 20 _ = error "We should've stopped by now"
+    go bitPosition report =
+      let gammaBin = gammaRate report
+          bitCriteria =
+            case rating of
+              O2Generator -> gammaBin
+              CO2Scrubber -> complement gammaBin
+       in case refine bitCriteria bitPosition report of
+            [rating] -> rating
+            [] -> error "Could not find rating"
+            refined -> go (bitPosition + 1) refined
+
 day3 :: Text -> IO (String, String)
 day3 input = do
   let report = parseInput input
       gammaBin = gammaRate report
       epsilonBin = complement gammaBin
-  return (show $ readBin gammaBin * readBin epsilonBin, "N/A")
+      o2rating = findRating O2Generator report
+      co2rating = findRating CO2Scrubber report
+  return (show $ readBin gammaBin * readBin epsilonBin, show $ readBin o2rating * readBin co2rating)
