72d555aca7c720fc1720ed4060b30bcb14d516e3
Martín Valdés de León <mvdl@amazon.de>
2021-12-04 23:11:25 +0100

    feat: day 2 part 2

--- src/Day2.hs
+++ src/Day2.hs
@@ -12,20 +12,27 @@ import Relude hiding (Down)
 data Command a = Forward a | Down a | Up a
   deriving (Show)
 
-data Submarine a = Submarine {_sDepth :: a, _sHPos :: a}
+data Submarine a = Submarine {_sDepth :: a, _sHPos :: a, _sAim :: a}
   deriving (Show)
 
 makeLenses ''Submarine
 
-applyCommand :: Num a => Command a -> Submarine a -> Submarine a
-applyCommand cmd sub =
+applyCommand :: Num a => Submarine a -> Command a -> Submarine a
+applyCommand sub cmd =
   case cmd of
     Forward a -> sub & sHPos +~ a
     Down a -> sub & sDepth +~ a
     Up a -> sub & sDepth -~ a
 
-processCommands :: Num a => (Command a -> Submarine a -> Submarine a) -> [Command a] -> Submarine a
-processCommands processor = foldr processor (Submarine 0 0)
+applyCommand' :: Num a => Submarine a -> Command a -> Submarine a
+applyCommand' sub cmd =
+  case cmd of
+    Forward a -> (sub & sHPos +~ a) & sDepth +~ (a * _sAim sub)
+    Down a -> sub & sAim +~ a
+    Up a -> sub & sAim -~ a
+
+processCommands :: Num a => (Submarine a -> Command a -> Submarine a) -> [Command a] -> Submarine a
+processCommands processor = foldl' processor (Submarine 0 0 0)
 
 parseCommand :: Parser a -> Parser (Command a)
 parseCommand pa = parseKeyword <* space <*> pa
@@ -39,7 +46,8 @@ parseInput input =
     Left err -> error $ fromString err
 
 day2 :: Text -> IO (String, String)
-day2 input = do
+day2 input =
   let commands = parseInput input
       part1 = processCommands applyCommand commands
-  return (show $ _sDepth part1 * _sHPos part1, "N/A")
+      part2 = processCommands applyCommand' commands
+   in return (show $ _sDepth part1 * _sHPos part1, show $ _sDepth part2 * _sHPos part2)
