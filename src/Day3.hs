0a39e3b66393ba848c7441ebe3a3eaa8ca146a8b
Martín Valdés de León <mvdl@amazon.de>
2021-12-05 01:03:26 +0100

    feat: Binary newtype for day 3

--- src/Day3.hs
+++ src/Day3.hs
@@ -9,8 +9,14 @@ import Data.List ((!!))
 import Numeric (readInt)
 import Relude
 
-parseInput :: Text -> [String]
-parseInput input = map toString $ lines input
+newtype Binary = Binary String
+  deriving (Show)
+
+unpack :: Binary -> String
+unpack (Binary number) = number
+
+parseInput :: Text -> [Binary]
+parseInput input = map (Binary . toString) $ lines input
 
 countBits :: Num a => String -> (a, a)
 countBits [] = (0, 0)
@@ -23,31 +29,31 @@ countBits (b : bs) =
 mostFrequent :: Ord a => (a, a) -> Char
 mostFrequent (zeroes, ones) = if zeroes > ones then '0' else '1'
 
-gammaRate :: [String] -> String
-gammaRate report = map (mostFrequent . countBits) (transpose report)
+gammaRate :: [Binary] -> Binary
+gammaRate report = Binary $ map (mostFrequent . countBits) (transpose $ map unpack report)
 
-complement :: String -> String
-complement = map flip
+complement :: Binary -> Binary
+complement (Binary number) = Binary $ map flip number
   where
     flip '0' = '1'
     flip '1' = '0'
     flip b = error $ "Unexpected bit value: " <> one b
 
-readBin :: Integral a => String -> a
-readBin number =
+readBin :: Integral a => Binary -> a
+readBin (Binary number) =
   case fmap fst . listToMaybe . readInt 2 (`elem` ("01" :: String)) digitToInt $ number of
     Just dec -> dec
     Nothing -> error $ "Unable to parse binary number: " <> toText number
 
-refine :: String -> Integer -> [String] -> [String]
-refine bitCriteria bitPosition = filter match
+refine :: Binary -> Integer -> [Binary] -> [Binary]
+refine (Binary bitCriteria) bitPosition = filter match
   where
-    match number = number !! fromInteger bitPosition == bitCriteria !! fromInteger bitPosition
+    match (Binary number) = number !! fromInteger bitPosition == bitCriteria !! fromInteger bitPosition
 
 data Rating = O2Generator | CO2Scrubber
   deriving (Show)
 
-findRating :: Rating -> [String] -> String
+findRating :: Rating -> [Binary] -> Binary
 findRating rating report = go 0 report
   where
     go 20 _ = error "We should've stopped by now"
