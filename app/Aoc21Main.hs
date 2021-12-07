7071b2dce4cf9301a9cbf6520174feb081865a84
Martín Valdés de León <mvdl@amazon.de>
2021-12-07 16:21:12 +0100

    feat: day 7

--- app/Aoc21Main.hs
+++ app/Aoc21Main.hs
@@ -9,6 +9,7 @@ import Day3 (day3)
 import Day4 (day4)
 import Day5 (day5)
 import Day6 (day6)
+import Day7 (day7)
 import Network.HTTP.Req ((/:))
 import qualified Network.HTTP.Req as R
 import qualified Options.Applicative as OA
@@ -17,7 +18,7 @@ import Relude
 import Session (session)
 import System.Directory (canonicalizePath, doesFileExist)
 
-data Day = Day1 | Day2 | Day3 | Day4 | Day5 | Day6
+data Day = Day1 | Day2 | Day3 | Day4 | Day5 | Day6 | Day7
   deriving (Show, Enum)
 
 dayId :: Day -> String
@@ -56,6 +57,7 @@ dayOption =
       <> pureCommand "day4" "Day 4: Giant Squid" Day4
       <> pureCommand "day5" "Day 5: Hydrothermal Venture" Day5
       <> pureCommand "day6" "Day 6: Lanternfish" Day6
+      <> pureCommand "day7" "Day 7: The Treachery of Whales" Day7
 
 pureCommand :: String -> String -> a -> OA.Mod OA.CommandFields a
 pureCommand cmd desc val = OA.command cmd (OA.info (pure val) (OA.progDesc desc))
@@ -84,6 +86,7 @@ main = do
     Day4 -> day4 input
     Day5 -> day5 input
     Day6 -> day6 input
+    Day7 -> day7 input
   putStrLn $ "Part 1: " <> part1
   putStrLn $ "Part 2: " <> part2
   where
---
+++ src/Day7.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Day7
+  ( day7,
+  )
+where
+
+import Data.Attoparsec.Text
+import qualified Data.List as L
+import Relude
+
+newtype Crab a = Crab {unCrab :: a}
+  deriving (Show, Eq, Ord, Num)
+
+parseCrab :: Parser a -> Parser (Crab a)
+parseCrab pa = Crab <$> pa
+
+parseInput :: Text -> [Crab Integer]
+parseInput input =
+  case parseOnly (parseCrab decimal `sepBy1` char ',') input of
+    Right ls -> ls
+    Left err -> error $ fromString err
+
+data Summary a = Summary {_sPos :: a, _sCount :: Integer, _sRest :: Integer}
+  deriving (Show)
+
+buildSummary :: [[Crab a]] -> [Summary a]
+buildSummary grouped =
+  let remaining = genericLength . concat $ grouped
+   in go 0 remaining grouped
+  where
+    go :: Integer -> Integer -> [[Crab a]] -> [Summary a]
+    go _ _ [] = []
+    go behind remaining (crabs : rest) =
+      let Crab a : _ = crabs
+          count = genericLength crabs
+          behind' = behind + count
+          remaining' = remaining - count
+       in (Summary a behind' remaining') : go behind' remaining' rest
+
+findPosition :: Ord a => [Crab a] -> a
+findPosition crabs =
+  let Just sa = L.find (\Summary {_sCount, _sRest} -> _sCount > _sRest) . buildSummary . L.group . L.sort $ crabs
+   in _sPos sa
+
+calculateFuel :: Num a => [Crab a] -> a -> a
+calculateFuel crabs a = sum $ map (abs . (a -) . unCrab) crabs
+
+calculateFuel' :: Integral a => [Crab a] -> a -> a
+calculateFuel' crabs a = sum $ map (scale . abs . (a -) . unCrab) crabs
+  where
+    scale n = n * (n + 1) `div` 2
+
+day7 :: Text -> IO (String, String)
+day7 input = do
+  let crabs = parseInput input
+      position = findPosition crabs
+      size = unCrab . L.maximum $ crabs
+      part2 = L.minimum . map (calculateFuel' crabs) $ [0 .. size]
+  return (show $ calculateFuel crabs position, show $ part2)
