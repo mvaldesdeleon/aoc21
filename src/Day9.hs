ccb2ec1fa0ee065de2bb5babfcb4808eff4aa2c9
Martín Valdés de León <mvdl@amazon.de>
2021-12-10 22:30:29 +0100

    feat: day 9 part 2

--- src/Day9.hs
+++ src/Day9.hs
@@ -8,23 +8,31 @@ import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 import Relude
 
-parseInput :: Text -> M.Map (Integer, Integer) Char
+type Position = (Integer, Integer)
+
+pTop, pBottom, pLeft, pRight :: Position -> Position
+pTop (y, x) = (y - 1, x)
+pBottom (y, x) = (y + 1, x)
+pLeft (y, x) = (y, x - 1)
+pRight (y, x) = (y, x + 1)
+
+parseInput :: Text -> M.Map Position Char
 parseInput input =
   let rows = lines input
       height = genericLength rows
       width = genericLength . toString . L.head $ rows
    in M.fromAscList $ zip [(y, x) | y <- [0 .. height - 1], x <- [0 .. width - 1]] (filter (/= '\n') . toString $ input)
 
-localMinimum :: M.Map (Integer, Integer) Char -> (Integer, Integer) -> Bool
-localMinimum floor (y, x) =
-  let val = floor M.! (y, x)
-      top = fromMaybe '9' $ floor M.!? (y -1, x)
-      left = fromMaybe '9' $ floor M.!? (y, x + 1)
-      bottom = fromMaybe '9' $ floor M.!? (y + 1, x)
-      right = fromMaybe '9' $ floor M.!? (y, x -1)
+localMinimum :: M.Map Position Char -> Position -> Bool
+localMinimum floor position =
+  let val = floor M.! position
+      top = fromMaybe '9' $ floor M.!? pTop position
+      left = fromMaybe '9' $ floor M.!? pLeft position
+      bottom = fromMaybe '9' $ floor M.!? pBottom position
+      right = fromMaybe '9' $ floor M.!? pRight position
    in val < top && val < left && val < bottom && val < right
 
-getLocalMinimum :: M.Map (Integer, Integer) Char -> [Char]
+getLocalMinimum :: M.Map Position Char -> [(Position, Char)]
 getLocalMinimum floor =
   let (top, left) = S.findMin $ M.keysSet floor
       (bottom, right) = S.findMax $ M.keysSet floor
@@ -32,11 +40,26 @@ getLocalMinimum floor =
   where
     maybeMinimum position =
       if localMinimum floor position
-        then Just $ floor M.! position
+        then Just $ (position, floor M.! position)
         else Nothing
 
+basinSize :: M.Map Position Char -> Position -> Integer
+basinSize floor position = go S.empty [position]
+  where
+    go visited (next : rest) =
+      if next `S.member` visited
+        then go visited rest
+        else
+          let value = fromMaybe '9' $ floor M.!? next
+           in if value == '9'
+                then go visited rest
+                else go (next `S.insert` visited) (pTop next : pLeft next : pBottom next : pRight next : rest)
+    go visited [] = toInteger . S.size $ visited
+
 day9 :: Text -> IO (String, String)
-day9 input = do
+day9 input =
   let floor = parseInput input
-      risk = map (+ 1) . mapMaybe (\x -> readMaybe [x]) . getLocalMinimum $ floor
-  return (show $ sum risk, "N/A")
+      lowPoints = getLocalMinimum $ floor
+      risk = map (+ 1) . mapMaybe (\(_, x) -> readMaybe [x]) $ lowPoints
+      basins = sortOn negate . map (basinSize floor . fst) $ lowPoints
+   in return (show $ sum risk, show $ product . take 3 $ basins)
